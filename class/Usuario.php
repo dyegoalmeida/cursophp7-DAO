26d33d66ad3789697e9c6d7bbefefd0cb2d5b86e
Dyego dos Santos Almeida <dyegocm@gmail.com>
2020-05-01 10:24:13 -0300

    Métodos de Lista

--- class/Usuario.php
+++ class/Usuario.php
@@ -57,8 +57,47 @@ class Usuario{
 		}
 	}
 
-	public function __toString(){
+	/*
+		Como não utilizamos o $this, esse método fica STATIC, na prática a vantagem é que
+		não precisamos instanciar o objeto para chamá-lo, apenas usar o USUARIO::NOMEMETODO
+	*/
+	public static function getList(){
 
+		$sql = new SQL();
+
+		return $sql->select("SELECT * FROM TB_USUARIOS ORDER BY DESLOGIN");
+	}
+
+	public static function search($login){
+
+		$sql = new SQL();
+
+		return $sql->select("SELECT * FROM TB_USUARIOS WHERE DESLOGIN LIKE :SEARCH ORDER BY IDUSUARIO", Array("SEARCH"=>"%".$login."%"));
+	}
+
+	public function login($login, $password){
+
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM TB_USUARIOS WHERE DESLOGIN = :LOGIN AND DESSENHA = :PASSWORD", array(":LOGIN"=>$login,
+										 ":PASSWORD"=>$password));
+		if (count($results) > 0) {
+
+			$row = $results[0];
+
+			$this->setIdusuario($row['idusuario']);
+			$this->setDeslogin($row['deslogin']);
+			$this->setDessenha($row['dessenha']);
+			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+
+		} else {
+
+			throw new Exception("Login e/ou Senha inválidos!Verifique!");
+		}
+	}
+
+	public function __toString(){
+        
 		return json_encode(array(
 			"idusuario"=>$this->getIdusuario(),
 			"deslogin"=>$this->getDeslogin(),
--- index.php
+++ index.php
@@ -2,17 +2,34 @@
 
 	require_once("config.php");
 
+	//$date = new DateTime('2020-04-30 13:21:12');
+	//echo $date->format('d/m/Y H:i:s');
+
+	$login = new Usuario();
+	$login->login("valter","0800963");
+
+	echo $login;
+
+	/*
+	$busca = Usuario::search("dy");
+	echo json_encode($busca);
+    */
+
+	/*
+	$lista = Usuario::getList();
+	echo json_encode($lista);
+    */
+	
+	//Carrega um usuário
+	/*
 	$root = new Usuario();
-
 	$root->loadbyID(4);
-
 	echo $root;
+	*/
 
 	/*
 	$sql = new Sql();
-
 	$usuarios = $sql->select("SELECT * FROM tb_usuarios");
-
 	echo json_encode($usuarios);
 	*/
 
